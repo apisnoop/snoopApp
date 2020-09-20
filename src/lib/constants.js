6161fbf3a0daf75ff7bedc113685b714c9931d01
zachmandeville <zz@ii.coop>
2020-09-21 11:41:14 +1200

    consume json from cncf/apisnoop/resources

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-export const releasesURL = 'https://raw.githubusercontent.com/apisnoop/snoopDB/master/resources/coverage';
+export const releasesURL = 'https://raw.githubusercontent.com/cncf/apisnoop/master/resources/coverage';
 export const RELEASES = [
   '1.20.0',
   '1.19.0',
